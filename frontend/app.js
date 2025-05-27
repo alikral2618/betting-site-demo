0d26c42de4b6edcfdfab1a33e2f18584a40bce17
Ali Mert <68358873+alikral2618@users.noreply.github.com>
2025-05-27 23:16:18 +0300

    Delete frontend directory

--- frontend/app.js
+++
@@ -1,8 +0,0 @@
-function play() {
-  const random = Math.random();
-  if (random > 0.7) {
-    document.getElementById('result').textContent = "Kazandın!";
-  } else {
-    document.getElementById('result').textContent = "Kaybettin.";
-  }
-}
