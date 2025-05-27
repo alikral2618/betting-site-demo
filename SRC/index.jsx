de97825a4e2bb0c69c44ae50c9175ee5a1732c9b
Ali Mert <68358873+alikral2618@users.noreply.github.com>
2025-05-27 23:37:40 +0300

    Delete SRC/index.jsx

--- SRC/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import GamePlatform from "./pages/GamePlatform";
-import "./styles/globals.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<GamePlatform />);
